d065f60d32b0a463db81162a8cfc132a6ea14482
jadanero <jadaneroc@gmail.com>
2026-03-03 17:05:03 +0100

    layoutPelicula v1.2

--- DSM_Proyecto_Web/src/App.tsx
+++ DSM_Proyecto_Web/src/App.tsx
@@ -1,5 +1,6 @@
-
 import './App.css'
+import LayoutPelicula from './Shared/Layout/LayoutPelicula';
+import { useState } from 'react';
 
 function App() {
 
@@ -8,11 +9,16 @@ function App() {
 
   return (
     <>
-      <p>
-        Hello World!
-      </p>
+      <LayoutPelicula
+        titulo="Kizoku Tensei"
+        imagenUrl="https://bocdn.ecotree.green/blog/0001/01/ad46dbb447cd0e9a6aeecd64cc2bd332b0cbcb79.jpeg?d=960x540"
+        generos={["Anime", "Aventura", "Fantasia"]}
+        rating={4.1}
+        votos={249}
+        sinopsis="Noah, el niño más fuerte del mundo..."
+      />
     </>
-    
+
   )
 }
 
