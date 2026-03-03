874a65de02e5bd6f768e0e07fe4ea61b3c4e93b7
jadanero <jadaneroc@gmail.com>
2026-03-03 17:07:54 +0100

    layoutPelicula.tsx + header + footer v1.0

--- DSM_Proyecto_Web/src/App.tsx
+++ DSM_Proyecto_Web/src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css'
 import LayoutPelicula from './Shared/Layout/LayoutPelicula';
+import Header from './Shared/Layout/Header';
+import Footer from './Shared/Layout/Footer';
 import { useState } from 'react';
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
 
   return (
     <>
+      <Header selectedLang={selectedLang} setSelectedLang={setSelectedLang} />
       <LayoutPelicula
         titulo="Kizoku Tensei"
         imagenUrl="https://bocdn.ecotree.green/blog/0001/01/ad46dbb447cd0e9a6aeecd64cc2bd332b0cbcb79.jpeg?d=960x540"
@@ -17,6 +20,7 @@ function App() {
         votos={249}
         sinopsis="Noah, el niño más fuerte del mundo..."
       />
+      <Footer />
     </>
 
   )
