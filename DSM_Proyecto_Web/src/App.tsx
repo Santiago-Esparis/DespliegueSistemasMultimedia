4f3b0bb39f488ac4d2475f89b6c5ab9eb8c9a659
jadanero <jadaneroc@gmail.com>
2026-03-03 16:43:47 +0100

    LayoutPelicula.tsx v1.1

--- DSM_Proyecto_Web/src/App.tsx
+++ DSM_Proyecto_Web/src/App.tsx
@@ -5,10 +5,14 @@ function App() {
 
   return (
     <>
-      <p>
-        Hello World!
-      </p>
-      <LayoutPelicula />
+      <LayoutPelicula
+      titulo="Kizoku Tensei"
+      imagenUrl="https://bocdn.ecotree.green/blog/0001/01/ad46dbb447cd0e9a6aeecd64cc2bd332b0cbcb79.jpeg?d=960x540"
+      generos={["Anime", "Acción"]}
+      rating={4.1}
+      votos={249}
+      sinopsis="Noah, el niño más fuerte del mundo..."
+    />
     </>
     
   )
--- DSM_Proyecto_Web/src/Shared/Layout/LayoutPelicula.tsx
+++ DSM_Proyecto_Web/src/Shared/Layout/LayoutPelicula.tsx
@@ -1,9 +1,57 @@
 import "./LayoutPelicula.css";
 
-export default function LayoutPelicula() {
-  return (
-    <div className="LayoutPelicula">
-      <h1>Hola Mundo</h1>
-    </div>
-  );
+type LayoutPeliculaProps = {
+    titulo: string;
+    imagenUrl: string;
+    generos: string[];
+    rating: number;
+    votos: number;
+    sinopsis: string;
+};
+
+export default function LayoutPelicula({
+    titulo,
+    imagenUrl,
+    generos,
+    rating,
+    votos,
+    sinopsis,
+}: LayoutPeliculaProps) {
+    return (
+        <div className="pelicula-container">
+
+            {/* HEADER */}
+            <div className="pelicula-header">
+
+                <div className="poster-container">
+                    <img src={imagenUrl} alt={titulo} />
+                </div>
+
+                <div className="pelicula-info">
+                    <h1>{titulo}</h1>
+
+                    <div className="peliculas-genero-block">
+                        {generos.map((genero, index) => (
+                            <span key={index} className="pelicula-genero">
+                                {genero}
+                            </span>
+                        ))}
+                    </div>
+                </div>
+
+                <div className="pelicula-rating">
+                    <div className="rating-number">{rating}</div>
+                    <div className="rating-votos">{votos} votos</div>
+                </div>
+
+            </div>
+
+            {/* SINOPSIS */}
+            <div className="pelicula-sinopsis">
+                <h2>Sinopsis</h2>
+                <p>{sinopsis}</p>
+            </div>
+
+        </div>
+    );
 }
